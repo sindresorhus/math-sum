edfbe5b305aeb88462c9fa73df1ca6d8f63d7556
Richie Bendall <richiebendall@gmail.com>
2025-07-13 19:30:52 +1200

    Meta tweak

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import test from 'ava';
-import mathSum from './index.js';
+import sum from './index.js';
 
 test('main', t => {
-	t.is(mathSum(1, 2, 3, 4), 10);
-	t.is(mathSum(1000, 1000), 2000);
-	t.is(mathSum([1, 2, 3, 4]), 10);
+	t.is(sum(1, 2, 3, 4), 10);
+	t.is(sum(1000, 1000), 2000);
+	t.is(sum([1, 2, 3, 4]), 10);
 });
